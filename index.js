6ba1d0eeb47a3052dfbe89a2f4ffac0628004e71
Mehedi4409 <sn096949@gmail.com>
2026-01-13 02:23:40 +0600

    call req fix 9

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // MIDDLEWARE
 // ==========================================
 // app.use(cors({ origin: process.env.LIVE_CLIENT_URL }));
-app.use(cors({ origin: "https://founders-academy-front.vercel.app" }));
+// app.use(cors({ origin: "https://founders-academy-front.vercel.app" }));
+app.use(cors({ origin: "https://www.thefounder.academy" }));
 app.use(express.json());
 
 // =====================
